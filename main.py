d277f55606e0e422ef796e7f6502a09331059c0f
lzha174 <min.oddo@gmail.com>
2021-09-16 18:30:24 +1200

    use multi threading and queue

--- main.py
+++ main.py
@@ -4,8 +4,24 @@ import requests
 from flask import Flask, render_template
 import vrp
 import data
+import os
+from queue import Queue
+from threading import Thread
+
 app = Flask(__name__)
 app.config['API_KEY'] = data.apik
+
+routenames = None
+routePoints = None
+googleMap = None
+
+def solveVRP(q):
+    routenames = vrp.solvevrp()
+    #routePoints = data.getStaticPoints()
+    routePoints = getAPILocations(routenames)
+    googleMap = GoogleMap(routePoints, routenames)
+    q.put((routenames, routePoints, googleMap))
+
 
 def extract_lat_long_via_address(address_or_zipcode):
     lat, lng = None, None
@@ -31,11 +47,14 @@ def extract_lat_long_via_address(address_or_zipcode):
 
 @app.route('/')
 def index():
+    global googleMap
+    while q.empty() == False:
+        values = q.get()
+        routenames = values[0]
+        routePoints = values[1]
+        googleMap = values[2]
 
-    routenames = vrp.solvevrp()
-    routePoints = data.getStaticPoints()
-    #routePoints = getAPILocations(routenames)
-    googleMap = GoogleMap(routePoints, routenames)
+
     context = {
         "key": app.config['API_KEY'],
         "title": 'VRT',
@@ -56,8 +75,10 @@ def getAPILocations(routenames):
             points.append(point)
         routePoints.append(points)
     return  routePoints
-import os
-if __name__=="__main__":
 
+if __name__=="__main__":
+    q = Queue()
+    thread1 = Thread(target=solveVRP, args=(q,), daemon=True)
+    thread1.start()
     app.run(host=os.getenv('IP', '0.0.0.0'),
             port=int(os.getenv('PORT', 4444)))
